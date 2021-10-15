b5d6871ae0b9a792cfe42bc238bdec67b80e99a8
Sean Terry <seanterry@Seans-MBP.localdomain>
2021-10-15 11:27:15 -0700

    Added Message to Home Landing page and a link to the order form

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,27 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 export default function Home (props){
     
 
     return (
-        <div>
+
+        <div className='home-wrapper'>
             <h2>================</h2>
-            <h2>Home.js</h2>
+            <img
+                className='home-image'
+                src='../public/Home-Pizza.jpg'
+                alt=''
+            />
+            <Link to="/pizza">Order Pizza</Link>
             <h2>================</h2>
         </div>
+
+
     )
 }
+
+
+
+// ### UPDATE ### Convert Div to have background
+
