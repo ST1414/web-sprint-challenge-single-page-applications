93914202701301b8a7736ff17a191330cf6cce9f
Sean Terry <seanterry@Seans-MBP.localdomain>
2021-10-15 09:03:23 -0700

    Import React Router and Yup, updated Index.js with Router tag, updated App.js with imports, create the OrderForm.js and formSchema.js files with bare minimum content.'

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
-import React from "react";
+// --- Add imports
+import React, {useState, useEffect} from "react"; 
+import axios from "axios";
+import * as yup from "yup";
+import OrderForm from "./OrderForm";
+import formSchema from "./validation/formSchema";
+
 
 const App = () => {
   return (
---
+++ src/OrderForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function OrderForm (props){
+
+
+    return (
+        <div>
+            <h2>OrderForm.js</h2>
+        </div>
+    )
+}
